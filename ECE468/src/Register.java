67ae9ea9eaf7cb9755f3f218d29a58d52492078f
Zhiyang Lin <lin382@ee207lnx12.ecn.purdue.edu>
2015-12-01 19:21:36 -0500

    ctrblock

---
+++ ECE468/src/Register.java
@@ -0,0 +1,12 @@
+import org.antlr.v4.runtime.*;
+public class Symbol {
+    public int num;
+    public String name;
+    public Reigster(String _num){
+            num = _num;
+    }
+
+    public create(String _num){
+
+    }
+}
