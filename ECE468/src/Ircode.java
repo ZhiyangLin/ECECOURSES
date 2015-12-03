0086198bcf9ffe23d81bf590361dc40bbd007c46
Zhiyang Lin <lin382@ee207lnx13.ecn.purdue.edu>
2015-12-02 19:37:44 -0500

    ircode changes

--- ECE468/src/Ircode.java
+++ ECE468/src/Ircode.java
@@ -5,50 +5,56 @@ public class Ircode {
 	public Irnode oprand2;
 	public Irnode result;
 	public int debug = 0;
+	public ArrayList<Ircode> successors = new ArrayList<Ircode>();
+	public ArrayList<Ircode> predecessors = new ArrayList<Ircode>();
+	public ArrayList<String> in = new ArrayList<String>();
+	public ArrayList<String> out = new ArrayList<String>();
+
 	public Ircode(String _opcode, Irnode _oprand1, Irnode _oprand2, Irnode _result){
 		opcode = _opcode;
 		oprand1 = _oprand1;
 		oprand2 = _oprand2;
 		result = _result;
 	}
-		public void printCode(){
-			if(oprand1.value.equals("none") && oprand2.value.equals("none") && result.value.equals("none")){
-				if(opcode.equals("END")){
-					System.out.println("");
-				}
-				else{
-					System.out.println(";" + opcode);
-				}
-			}
-			else if(oprand1.value.equals("none") && oprand2.value.equals("none")){
-				System.out.printf(";%s %s \n",opcode, result.value);
-			}
-			else if(oprand2.value.equals("none")){
-				System.out.printf(";%s %s %s\n",opcode, oprand1.value, result.value);	
+
+	public void printCode(){
+		if(oprand1.value.equals("none") && oprand2.value.equals("none") && result.value.equals("none")){
+			if(opcode.equals("END")){
+				System.out.println("");
 			}
 			else{
-				System.out.printf(";%s %s %s %s\n",opcode, oprand1.value, oprand2.value, result.value);
+				System.out.println(";" + opcode);
 			}
 		}
-	
-		public void deprintCode(){
-			if(oprand1.value.equals("none") && oprand2.value.equals("none") && result.value.equals("none")){
-				if(opcode.equals("END")){
-					System.out.println("");
-				}
-				else{
-					System.out.println(";" + opcode);
-				}
-			}
-			else if(oprand1.value.equals("none") && oprand2.value.equals("none")){
-				System.out.printf(";%s %s(%s) \n",opcode, result.value,result.gtype);
-			}
-			else if(oprand2.value.equals("none")){
-				System.out.printf(";%s %s(%s) %s(%s)\n",opcode, oprand1.value,oprand1.gtype, result.value,result.gtype);	
+		else if(oprand1.value.equals("none") && oprand2.value.equals("none")){
+			System.out.printf(";%s %s \n",opcode, result.value);
+		}
+		else if(oprand2.value.equals("none")){
+			System.out.printf(";%s %s %s\n",opcode, oprand1.value, result.value);	
+		}
+		else{
+			System.out.printf(";%s %s %s %s\n",opcode, oprand1.value, oprand2.value, result.value);
+		}
+	}
+
+	public void deprintCode(){
+		if(oprand1.value.equals("none") && oprand2.value.equals("none") && result.value.equals("none")){
+			if(opcode.equals("END")){
+				System.out.println("");
 			}
 			else{
-				System.out.printf(";%s %s(%s) %s(%s) %s(%s)\n",opcode, oprand1.value,oprand1.gtype, oprand2.value,oprand2.gtype, result.value,result.gtype);
+				System.out.println(";" + opcode);
 			}
 		}
+		else if(oprand1.value.equals("none") && oprand2.value.equals("none")){
+			System.out.printf(";%s %s(%s) \n",opcode, result.value,result.gtype);
+		}
+		else if(oprand2.value.equals("none")){
+			System.out.printf(";%s %s(%s) %s(%s)\n",opcode, oprand1.value,oprand1.gtype, result.value,result.gtype);	
+		}
+		else{
+			System.out.printf(";%s %s(%s) %s(%s) %s(%s)\n",opcode, oprand1.value,oprand1.gtype, oprand2.value,oprand2.gtype, result.value,result.gtype);
+		}
+	}
 	
 }
